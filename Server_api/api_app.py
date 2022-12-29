9f856a2bc310b272ba002631e2164c2ba54b2932
David8344 <josedavid8344@gmail.com>
2022-12-29 14:30:30 -0300

    todos de usuario funcionando

--- Server_api/api_app.py
+++ Server_api/api_app.py
@@ -1,19 +1,56 @@
-from flask import Flask, .
-from jsons.data import *
+from flask import Flask, jsonify
+import json
+
+
+  
+
   
 
 app = Flask(__name__)
 app.debug = True
-
-
+#carregue o arquivo todos.json e salve na variável todos_obj
+todos_obj = json.load(open('todos.json'))
+#carregue o arquivo users.json e salve na variável users_obj
+users_obj = json.load(open('users.json'))
 
 @app.route('/')
 def index():
-  #retorne a lista de users vindo do arquivo data.py
-  return jsonify(users)
+    
+    #retorne a variável todos_obj
+    return todos_obj
+
+@app.route('/todos')
+def todos():
+    #retorne a variável todos_obj
+    return todos_obj
+    
+@app.route('/users')
+def users():
+    
+    #retorne a variável users_obj
+    return users_obj
+
+
 
 @app.route('/users/<int:user_id>')
-def users(user_id):
-    return users[user_id+1]
-    
+def user(user_id):
+    return users_obj[user_id-1]
+
+@app.route("/users/<int:userid>/todos")
+def usertodos(userid):
+    #crie uma lista vazia chamada user_todos
+    user_todos = []
+    #percorra todos os itens da lista todos_obj
+    for todo in todos_obj:
+        #se o id do usuário do item da lista todos_obj for igual ao userid passado na url
+        if todo['userId'] == userid-1:
+            #adicione o item da lista todos_obj na lista user_todos
+            user_todos.append(todo)
+    #retorne a lista user_todos
+    return user_todos
+
+@app.route('/todos/<int:todo_id>')
+def todo(todo_id):
+    #retorne o item da lista todos_obj que tem o id igual ao todo_id passado na url
+        return todos_obj[todo_id-1]
 app.run(debug=True, port=5000)
